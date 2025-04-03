24773d74e1a9441d8384513ae73037470e7f753c
Kadyr <niazkadyrov228@gmail.com>
2025-04-04 04:05:56 +0500

    add review server

---
+++ review/cmd/main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"review/internal/config"
+	"review/internal/handlers/create"
+	del "review/internal/handlers/delete"
+	"review/internal/handlers/get"
+	"review/internal/handlers/update"
+	kafka "review/internal/kafka/producer"
+	"review/internal/lib/logger/sl"
+	mwAuth "review/internal/middleware/auth"
+	mwLogger "review/internal/middleware/logger"
+	"review/internal/storage/cache"
+	"review/internal/storage/db"
+	"review/pkg/token"
+	"syscall"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+
+	cfg := config.LoadConfig()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info(
+		"starting review-service",
+		slog.String("env", cfg.Env),
+	)
+
+	log.Debug("debug messages are enabled")
+
+	kafkaProducer, err := kafka.NewProducer(
+		[]string{cfg.KafkaBroker},
+		cfg.KafkaTopic,
+		log,
+	)
+
+	if err != nil {
+		log.Error("failed to initialize Kafka producer", sl.Err(err))
+	}
+
+	defer kafkaProducer.Close()
+
+	storage, err := db.NewStorage(cfg.Config)
+	if err != nil {
+		log.Error("error created storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	redisClient := cache.New(cfg.RedisConfig)
+
+	redisRepository := cache.NewRedisRepository(redisClient)
+
+	tokenMn, err := token.NewTokenManagerRSA(cfg.PublicKeyPath)
+	if err != nil {
+		log.Error("error created a new token manager", sl.Err(err))
+		os.Exit(1)
+	}
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.URLFormat)
+	router.Use(mwLogger.New(log))
+
+	router.Group(func(r chi.Router) {
+		r.Use(mwAuth.AuthMiddleware(tokenMn, log))
+		r.Post("/review/create", create.Create(log, storage, kafkaProducer))
+		r.Put("/review/update", update.Update(log, storage, kafkaProducer))
+		r.Delete("/review/delete/{id}", del.Delete(log, storage, kafkaProducer))
+
+	})
+
+	router.Get("/review/get", get.Get(log, storage, redisRepository))
+
+	log.Info("starting server", slog.String("adsress", cfg.Address))
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	srv := &http.Server{
+		Addr:         cfg.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.Timeout,
+		WriteTimeout: cfg.Timeout,
+		IdleTimeout:  cfg.IdleTimeout,
+	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Error("failed to start server", sl.Err(err))
+		}
+	}()
+
+	log.Info("server started")
+
+	<-done
+	log.Info("stopping server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Error("failed to stop server", sl.Err(err))
+
+		return
+	}
+
+	log.Info("server stopped")
+
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	default:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
+}
---
+++ review/internal/handlers/create/create.go
@@ -0,0 +1,97 @@
+package create
+
+import (
+	"log/slog"
+	"net/http"
+	"review/internal/domain/model"
+	"review/internal/domain/response"
+	"review/internal/lib/logger/sl"
+	"review/internal/lib/validate"
+	mwAuth "review/internal/middleware/auth"
+	"review/pkg/token"
+	"time"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
+)
+
+type ReviewCreater interface {
+	CreateReview(review *model.Review) (int64, error)
+	IfExist(userID int64, mentorEmail string) (bool, error)
+}
+
+type KafkaProducer interface {
+	SendReviewEvent(review *model.ReviewEvent) error
+}
+
+func Create(log *slog.Logger, reviewCreater ReviewCreater, kafkaProducer KafkaProducer) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.create.Create"
+		log := log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		claims := r.Context().Value(mwAuth.UserKey).(*token.Claims)
+
+		var req model.Review
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("failed to decode request body", sl.Err(err))
+			render.Status(r, http.StatusBadRequest)
+			render.JSON(w, r, response.Error("invalid request body"))
+			return
+		}
+
+		req.UserID = claims.UserID
+
+		if err := validate.IsValid(req); err != nil {
+			log.Error("validation error", sl.Err(err))
+			render.Status(r, http.StatusBadRequest)
+			render.JSON(w, r, response.Error("server error"))
+			return
+		}
+
+		exist, err := reviewCreater.IfExist(req.UserID, req.MentorEmail)
+		if err != nil {
+			log.Error("falied to find review", sl.Err(err))
+			render.Status(r, http.StatusInternalServerError)
+			render.JSON(w, r, response.Error("server error"))
+			return
+		}
+
+		if exist {
+			log.Warn("review already exist")
+			render.Status(r, http.StatusConflict)
+			render.JSON(w, r, map[string]any{
+				"status": "review already exist",
+			})
+			return
+		}
+
+		req.CreatedAt = time.Now()
+		id, err := reviewCreater.CreateReview(&req)
+		if err != nil {
+			log.Error("falied to create review", sl.Err(err))
+			render.Status(r, http.StatusInternalServerError)
+			render.JSON(w, r, response.Error("server error"))
+			return
+		}
+
+		event := &model.ReviewEvent{
+			Action: "updated",
+			ID:     id,
+			Email:  req.MentorEmail,
+			Score:  req.Rating,
+		}
+
+		if err := kafkaProducer.SendReviewEvent(event); err != nil {
+			log.Error("failed to send kafka even", sl.Err(err))
+		}
+
+		render.Status(r, http.StatusCreated)
+		render.JSON(w, r, map[string]any{
+			"id": id,
+		})
+	}
+}
